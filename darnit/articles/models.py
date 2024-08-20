5b2bb8744ab2c7fa2170d2444ad6a33be2e1cb5b
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-08-20 09:53:41 +0000

    made migrations

--- darnit/articles/models.py
+++ darnit/articles/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    title
-    slug
-    body
-    date
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
     #add thumbnail
