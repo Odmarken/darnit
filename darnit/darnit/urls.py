fe9cfb6e3ad112f06e326552adada30358c4b5a5
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-08-21 16:55:46 +0000

    added background color green to homepage

--- darnit/darnit/urls.py
+++ darnit/darnit/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, re_path, include  
 from django.contrib import admin
+from django.urls import path, re_path, include 
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),  
     path('articles/', include('articles.urls')),  
-    re_path(r'^about/$', views.about),  #
+    path('about/', views.about),  
     path('', views.homepage),  
 ]
+
+urlpatterns += staticfiles_urlpatterns()
